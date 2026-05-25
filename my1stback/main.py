6cedb79641478048ce6e967b6d3977befd4c43d6
kobayashi <hiroyuki.kobayashi@oit.ac.jp>
2026-05-25 15:38:44 +0900

    improc back is implemented

--- my1stback/main.py
+++ my1stback/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, Response
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import cv2
+import numpy as np
 
 class CalcParams(BaseModel):
     operator: str
@@ -7,7 +10,11 @@ class CalcParams(BaseModel):
     operand2: float
 
 app = FastAPI()
-
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
 
 @app.get("/")
 def read_root():
@@ -35,3 +42,21 @@ def post_calc(params: CalcParams):
     
     return {"ans": ans}
 
+@app.post("/improc")
+async def improc(image_file: UploadFile,
+                 procedure: str):
+    data = await image_file.read()
+    arr = np.frombuffer(data, np.uint8)
+    img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if procedure == 'gray':
+        img2 = gray
+    elif procedure == 'edge':
+        img2 = cv2.Canny(gray, 100, 200)
+    elif procedure == 'illust':
+        img2 = cv2.stylization(img, sigma_s=60, sigma_r=0.07)
+    else:
+        img2 = img
+    _, enc_img = cv2.imencode(".jpg", img2)
+    return Response(content=enc_img.tobytes(),
+                    media_type='image/jpeg')
